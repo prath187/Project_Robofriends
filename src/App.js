912f121acd0d5ef36ff3842e21726ccbac3f49c1
Prathmesh Dodake <prathmeshdodk06@gmail.com>
2022-03-19 19:45:15 +0530

    class basics

---
+++ src/App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+
+class Home extends React.Component {
+  render(){
+
+  
+    return (
+      <h1>hello world</h1>
+    );
+  }
+}
+
+
+
+export default Home;
+
